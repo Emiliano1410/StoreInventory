41f1f34ddb5ef98259c903f178c055190d0d5b24
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2024-05-21 13:08:28 -0600

    Create Electronic.h

---
+++ Electronic.h
@@ -0,0 +1,22 @@
+#ifndef ELECTRONIC_H
+#define ELECTRONIC_H
+
+#include "Product.h"
+
+class Electronic : public Product {
+private:
+    // Periodo de garantía del producto en meses
+    int warrantyPeriod;
+
+public:
+    Electronic(const std::string& name, double price, int quantity, int warrantyPeriod);
+
+    // Métodos getter y setter para el periodo de garantía
+    int getWarrantyPeriod() const;
+    void setWarrantyPeriod(int warrantyPeriod);
+
+    // Método sobrescrito para imprimir los detalles del producto electrónico.
+    void printDetails() const override;
+};
+
+#endif
