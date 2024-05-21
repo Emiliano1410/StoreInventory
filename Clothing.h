d9c5b4af6cad4eb954596f4a1bb68473d55c0d11
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2024-05-21 13:09:08 -0600

    Create Clothing.h

---
+++ Clothing.h
@@ -0,0 +1,26 @@
+#ifndef CLOTHING_H
+#define CLOTHING_H
+
+#include "Product.h"
+
+class Clothing : public Product {
+private:
+    // Talla de la ropa
+    std::string size;
+    // Material de la ropa
+    std::string material;
+
+public:
+    Clothing(const std::string& name, double price, int quantity, const std::string& size, const std::string& material);
+
+    // Métodos getter y setter para la talla y el material
+    std::string getSize() const;
+    void setSize(const std::string& size);
+    std::string getMaterial() const;
+    void setMaterial(const std::string& material);
+
+    // Método sobrescrito para imprimir los detalles de la prenda de ropa.
+    void printDetails() const override;
+};
+
+#endif
