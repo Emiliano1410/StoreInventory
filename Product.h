94e97a9f75deea0501a14ca13b9961dea956e49a
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2024-05-21 13:06:13 -0600

    Create Product.h

---
+++ Product.h
@@ -0,0 +1,34 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include <string>
+
+class Product {
+protected:
+    // Nombre del producto
+    std::string name;
+    // Precio del producto
+    double price;
+    // Cantidad del producto en el inventario
+    int quantity;
+
+public:
+    Product(const std::string& name, double price, int quantity);
+    // Destructor virtual
+    virtual ~Product() {}
+
+    // Métodos getter y setter para los atributos del producto
+    std::string getName() const;
+    void setName(const std::string& name);
+
+    double getPrice() const;
+    void setPrice(double price);
+
+    int getQuantity() const;
+    void setQuantity(int quantity);
+    
+    // Método virtual para imprimir los detalles del producto
+    virtual void printDetails() const;
+};
+
+#endif
