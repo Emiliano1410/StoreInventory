72a49bf831d7fa7d020c1f4cb72cc752acbedb22
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2024-05-24 14:09:28 -0600

    Update Store.h

--- Store.h
+++ Store.h
@@ -3,6 +3,9 @@
 
 #include "Inventory.h"
 #include "Product.h"
+#include "Fruit.h"
+#include "Electronic.h"
+#include "Clothing.h"
 #include <memory>
 
 class Store {
@@ -12,8 +15,13 @@ private:
 
 public:
     Store();
-    // Agrega un producto al inventario de la tienda
+
+    // Sobrecargar addProductToInventory para diferentes tipos de productos
     void addProductToInventory(const std::shared_ptr<Product>& product);
+    void addProductToInventory(const std::string& name, double price, int quantity, const std::string& origin);
+    void addProductToInventory(const std::string& name, double price, int quantity, int warrantyPeriod);
+    void addProductToInventory(const std::string& name, double price, int quantity, const std::string& size, const std::string& material);
+
     // Elimina un producto del inventario de la tienda por su nombre.
     void removeProductFromInventory(const std::string& productName);
     // Lista todos los productos en el inventario.
