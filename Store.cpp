4003ed2b094559a21ed795510af925ba1ee72820
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2024-05-24 14:10:07 -0600

    Update Store.cpp

--- Store.cpp
+++ Store.cpp
@@ -7,6 +7,21 @@ void Store::addProductToInventory(const std::shared_ptr<Product>& product) {
     inventory.addProduct(product);
 }
 
+void Store::addProductToInventory(const std::string& name, double price, int quantity, const std::string& origin) {
+    auto product = std::make_shared<Fruit>(name, price, quantity, origin);
+    inventory.addProduct(product);
+}
+
+void Store::addProductToInventory(const std::string& name, double price, int quantity, int warrantyPeriod) {
+    auto product = std::make_shared<Electronic>(name, price, quantity, warrantyPeriod);
+    inventory.addProduct(product);
+}
+
+void Store::addProductToInventory(const std::string& name, double price, int quantity, const std::string& size, const std::string& material) {
+    auto product = std::make_shared<Clothing>(name, price, quantity, size, material);
+    inventory.addProduct(product);
+}
+
 void Store::removeProductFromInventory(const std::string& productName) {
     inventory.removeProduct(productName);
 }
