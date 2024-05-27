329ebca4e6bfc54f48840ee58d8f5f0064f85c25
Emiliano1410 <142271390+Emiliano1410@users.noreply.github.com>
2024-05-27 15:34:26 -0600

    Update Store.cpp

--- Store.cpp
+++ Store.cpp
@@ -1,38 +1,37 @@
 #include "Store.h"
+#include <algorithm>
 #include <iostream>
 
 Store::Store() {}
 
 void Store::addProductToInventory(const std::shared_ptr<Product>& product) {
-    inventory.addProduct(product);
+    products.push_back(product);
 }
 
 void Store::addProductToInventory(const std::string& name, double price, int quantity, const std::string& origin) {
     auto product = std::make_shared<Fruit>(name, price, quantity, origin);
-    inventory.addProduct(product);
+    products.push_back(product);
 }
 
 void Store::addProductToInventory(const std::string& name, double price, int quantity, int warrantyPeriod) {
     auto product = std::make_shared<Electronic>(name, price, quantity, warrantyPeriod);
-    inventory.addProduct(product);
+    products.push_back(product);
 }
 
 void Store::addProductToInventory(const std::string& name, double price, int quantity, const std::string& size, const std::string& material) {
     auto product = std::make_shared<Clothing>(name, price, quantity, size, material);
-    inventory.addProduct(product);
+    products.push_back(product);
 }
 
 void Store::removeProductFromInventory(const std::string& productName) {
-    inventory.removeProduct(productName);
+    products.erase(std::remove_if(products.begin(), products.end(),
+                                  [&productName](const std::shared_ptr<Product>& product) {
+                                      return product->getName() == productName;
+                                  }), products.end());
 }
 
 void Store::listProducts() const {
-    const auto& products = inventory.getProducts();
     for (const auto& product : products) {
         product->printDetails();
     }
-}
-
-const Inventory& Store::getInventory() const {
-    return inventory;
 }
